55f9a8eb0b011bdbe1daa4819529fdd8902d27b4
Ian Baron <ian.baron@manchester.ac.uk>
2024-02-14 11:40:46 +0000

    style which header route we're on

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
 const router = useRouter()
 
+const currentPath = computed(() => {
+  return router.currentRoute.value.path
+})
+
 const { data: countOfRecipes } = await useAsyncData('recipeCount', () =>
   queryContent('/recipes').count())
 
 async function navToRandomRecipe() {
   const randomIndex = Math.floor(Math.random() * Number(countOfRecipes.value))
   const randomSlugContent = await queryContent('/recipes').skip(randomIndex).limit(1).only('slug').find()
-  if (router.currentRoute.value.path === `/recipe/${randomSlugContent[0].slug}`) {
+  if (currentPath.value === `/recipe/${randomSlugContent[0].slug}`) {
     navToRandomRecipe()
   }
   router.push(`/recipe/${randomSlugContent[0].slug}`)
 }
+
+const targetLatest: string = '/'
+const targetAll: string = '/all-recipes'
+const targetAdd: string = '/add-recipe'
 </script>
 
 <template>
@@ -20,28 +28,32 @@ async function navToRandomRecipe() {
       <button
         type="button"
         class="text-white text-xl bg-blue-gray-500 rounded-lg text-center font-medium md:text-3xl px-5 py-2.5 me-2 mb-2"
-        @click="$router.push('/')"
+        :class="{ 'opacity-50': currentPath !== targetLatest }"
+        @click="$router.push(targetLatest)"
       >
         Latest
       </button>
       <button
         type="button"
-        class="text-white bg-blue-gray-500 font-medium rounded-lg text-xl md:text-3xl px-5 py-2.5 text-center me-2 mb-2"
-        @click="$router.push('/all-recipes')"
+        class="text-white text-xl bg-blue-gray-500 rounded-lg text-center font-medium md:text-3xl px-5 py-2.5 me-2 mb-2"
+        :class="{ 'opacity-50': currentPath !== targetAll }"
+        @click="$router.push(targetAll)"
       >
         All
       </button>
       <button
         type="button"
-        class="text-white bg-blue-gray-500 font-medium rounded-lg text-xl md:text-3xl px-5 py-2.5 text-center me-2 mb-2"
+        class="text-white text-xl bg-blue-gray-500 rounded-lg text-center font-medium md:text-3xl px-5 py-2.5 me-2 mb-2"
+        :class="{ 'opacity-50': !currentPath.startsWith('/recipe/') }"
         @click="navToRandomRecipe"
       >
         Random
       </button>
       <button
         type="button"
-        class="text-white bg-blue-gray-500 font-medium rounded-lg text-xl md:text-3xl px-5 py-2.5 text-center me-2 mb-2"
-        @click="$router.push('/add-recipe')"
+        class="text-white text-xl bg-blue-gray-500 rounded-lg text-center font-medium md:text-3xl px-5 py-2.5 me-2 mb-2"
+        :class="{ 'opacity-50': currentPath !== targetAdd }"
+        @click="$router.push(targetAdd)"
       >
         <Icon name="lucide:plus" class="scale-140" />
       </button>
