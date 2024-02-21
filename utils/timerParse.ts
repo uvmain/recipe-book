36a8ac9cd1c1419954e78ee9b1cf4a00a17d14a6
Ian Baron <ian.baron@manchester.ac.uk>
2024-02-21 09:57:45 +0000

    better function naming

--- utils/timerParse.ts
+++ utils/timerParse.ts
@@ -1,42 +1,47 @@
-let timerArray: number[] = []
+let minutesArray: number[] = []
 
-function addToTimerArray(amount: string, word: string) {
+function clearMinutesArray() {
+  minutesArray = []
+}
+
+function addToMinutesArray(amount: string, word: string) {
   if (Number(amount))
-    timerArray.push(word.includes('hour') ? Number(amount) * 60 : Number(amount))
+    minutesArray.push(word.includes('hour') ? Number(amount) * 60 : Number(amount))
 }
 
 export function timerParse(recipeInstructions: string): number[] {
-  timerArray = []
+  clearMinutesArray()
+
   const recipesWords = recipeInstructions.toLowerCase().split(' ')
 
   recipesWords.forEach((word: string, index) => {
     if (word.includes('min') || word.includes('hour')) {
       const prevWord = `${recipesWords[index - 1]}`.replaceAll('½', '.5').replaceAll('¾', '.75')
       if (prevWord === 'few') {
-        addToTimerArray('3', word)
+        addToMinutesArray('3', word)
       }
       else if (prevWord.startsWith('~')) {
-        addToTimerArray(prevWord.replace('~', ''), word)
+        addToMinutesArray(prevWord.replace('~', ''), word)
       }
       else if (prevWord === 'of' && recipesWords[index - 2] === 'couple') {
-        addToTimerArray('2', word)
+        addToMinutesArray('2', word)
       }
       else if (prevWord.includes('-')) {
-        addToTimerArray(prevWord.split('-').slice(0)[0], word)
-        addToTimerArray(prevWord.split('-').slice(-1)[0], word)
+        addToMinutesArray(prevWord.split('-').slice(0)[0], word)
+        addToMinutesArray(prevWord.split('-').slice(-1)[0], word)
       }
       else if (prevWord === 'further' && !word.endsWith('s')) {
-        addToTimerArray('1', word)
+        addToMinutesArray('1', word)
       }
       else if (Number(prevWord) && ['to', 'or'].includes(recipesWords[index - 2])) {
-        addToTimerArray(recipesWords[index - 3], word)
-        addToTimerArray(prevWord, word)
+        addToMinutesArray(recipesWords[index - 3], word)
+        addToMinutesArray(prevWord, word)
       }
       else {
-        addToTimerArray(prevWord, word)
+        addToMinutesArray(prevWord, word)
       }
     }
   })
 
-  return timerArray
+  return minutesArray
 }
