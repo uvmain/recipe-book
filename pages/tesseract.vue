5e6d1fd20ae13a5e58e2081c59b8678eb370e4cc
Ian Baron <ian.baron@manchester.ac.uk>
2024-02-22 17:38:00 +0000

    rework ocr

--- pages/tesseract.vue
+++ pages/tesseract.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
-import { PSM, createWorker } from 'tesseract.js'
+import { OEM, PSM, createWorker } from 'tesseract.js'
 
-const reognisedText = ref('')
+const recognizedParagraphs = ref<string[]>([])
 
 async function recognize() {
-  const worker = await createWorker('eng')
+  recognizedParagraphs.value = []
+  const worker = await createWorker('eng', OEM.DEFAULT)
 
   await worker.setParameters({
     tessedit_pageseg_mode: PSM.AUTO_OSD,
+    preserve_interword_spaces: '1',
   })
 
   const img = document.getElementById('text-img') as HTMLImageElement
 
   if (img) {
-    const { data: { text } } = await worker.recognize(img)
-    reognisedText.value = text
+    const { data: { paragraphs } } = await worker.recognize(img)
+    paragraphs.forEach((paragraph) => {
+      if (paragraph.text.replaceAll('\n', '').length > 2)
+        recognizedParagraphs.value.push(paragraph.text)
+    })
   }
 
   await worker.terminate()
@@ -22,14 +27,19 @@ async function recognize() {
 </script>
 
 <template>
-  <div class="font-sans antialiased text-center text-bluegray-700 mt-60">
-    <button @click="recognize">
-      recognize
-    </button>
-    <img id="text-img" alt="Vue logo" src="/ocr-test.jpg">
-    <br>
-    <div v-if="reognisedText">
-      {{ reognisedText }}
+  <div class="font-sans antialiased text-center text-bluegray-700 mt-10">
+    <div class="grid grid-cols-2">
+      <img id="text-img" alt="Vue logo" src="/ocr-test.jpg" class="mx-4 max-w-full ml-auto border-solid border-gray-300 max-h-3/4">
+      <div class="ml-4 text-left">
+        <button class="p-2 mb-4" @click="recognize">
+          recognize
+        </button>
+        <div v-if="recognizedParagraphs.length" class="text-white">
+          <div v-for="(text, index) in recognizedParagraphs" :key="index">
+            {{ text }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
