4c3d8444a67363b4f449a907b33852a0ae1b1683
Ian Baron <ian.baron@manchester.ac.uk>
2024-03-15 15:25:01 +0000

    better responsiveness for Recipe.vue

--- components/Recipe.vue
+++ components/Recipe.vue
@@ -17,15 +17,16 @@ const caloriesPerServing = computed(() => {
     <h2 class="font-bold text-center mb-4 text-4xl text-zinc-600">
       {{ recipe.name }}
     </h2>
-    <div class="flex items-center mb-4 gap-3 md:mb-6 mx-auto md:w-4/5">
+    <div class="mb-4 flex items-center gap-3 md:mb-6 mx-auto md:w-4/5">
       <div class="w-full h-0.5 bg-gradient-to-r to-zinc-500 from-primarybg-300" />
       <RecipeIcons :recipe="recipe" />
       <div class="w-full h-0.5 to-zinc-500 from-primarybg-300 bg-gradient-to-l" />
     </div>
-    <div class="justify-center grid auto-cols-auto gap-4 md:gap-8 lg:grid-flow-col">
-      <div class="">
-        <div class="mb-4 flex rounded-md bg-blue-gray-500 items-baseline justify-between p-4 pt-0">
-          <div>
+
+    <div class="justify-center flex flex-col md:flex-row gap-4">
+      <div class="min-w-1/3 lg:max-w-1/2">
+        <div class="mb-4 flex flex-col md:flex-row gap-4 rounded-md bg-blue-gray-500 items-baseline justify-between p-4 pt-0">
+          <div class="text-center md:text-left mx-auto md:mx-0">
             <p v-if="recipe.author">
               <strong>Author:</strong>
               {{ recipe.author }}
@@ -35,7 +36,7 @@ const caloriesPerServing = computed(() => {
               {{ recipe.source }}
             </component>
           </div>
-          <div class="grid text-sm text-right opacity-80 min-w-1/5">
+          <div class="grid text-sm text-center md:text-right opacity-80 min-w-1/5 mx-auto lg:mx-0">
             <span v-if="recipe.servings">{{ recipe.servings }} Servings</span>
             <span v-if="recipe.prepTime">Prep: {{ recipe.prepTime }}</span>
             <span v-if="recipe.cookingTime">Cook: {{ recipe.cookingTime }}</span>
@@ -44,7 +45,7 @@ const caloriesPerServing = computed(() => {
         </div>
         <img :src="recipe.image" :alt="recipe.name" class="rounded-lg w-full shadow-md h-auto md:mb-4">
       </div>
-      <div>
+      <div class="lg:max-w-5/9">
         <div class="grid gap-4">
           <MarkdownBlock v-if="recipe.ingredients" label="Ingredients" :markdown-string="recipe.ingredients" props-class="bg-blue-gray-600" class="min-w-1/2" />
           <MarkdownBlock v-if="recipe.instructions" label="Instructions" :markdown-string="recipe.instructions" props-class="bg-gray-600" class="min-w-1/2" />
