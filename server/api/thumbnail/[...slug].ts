dfd335646a42faa3495bce7527c04b09ea302152
Ian Baron <uv.fragm@gmail.com>
2024-07-12 15:58:56 +0100

    add caching to image APIs

--- server/api/thumbnail/[...slug].ts
+++ server/api/thumbnail/[...slug].ts
@@ -1,23 +1,36 @@
-const baseUrl = process.env.DIRECTUS_URL as string
-const token = process.env.DIRECTUS_TOKEN as string
+const baseUrl = process.env.DIRECTUS_URL as string;
+const token = process.env.DIRECTUS_TOKEN as string;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cache: { [key: string]: any } = {};
 
 export default defineEventHandler(async (event) => {
-  const slug = event.context.params?.slug
-  const url = `${baseUrl}/assets/${slug}?width=400&height=400&fit=inside`
+  const slug = event.context.params?.slug;
+
+  if (!slug) {
+    return { error: 'Slug is undefined' };
+  }
+
+  const cacheKey = Array.isArray(slug) ? slug.join('/') : slug;
+  const url = `${baseUrl}/assets/${cacheKey}?width=400&height=400&fit=inside`;
+
+  if (cache[cacheKey]) {
+    return cache[cacheKey];
+  }
 
   try {
     const response = await $fetch(url, {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-    })
-    .catch((error) => {
-      throw new Error(`Failed to fetch data: ${JSON.stringify(error.data)}`)
+    }).catch((error) => {
+      throw new Error(`Failed to fetch data: ${JSON.stringify(error.data)}`);
     });
 
-    return response
+    cache[cacheKey] = response;
+
+    return response;
   }
  catch (error) {
-    return { error: `Failed to fetch data from CMS: ${error}` }
+    return { error: `Failed to fetch data from CMS: ${error}` };
   }
-})
+});
