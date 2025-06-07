5b5d8bf3f1ba2e0f40d038a39caf63a107739f15
Ian Baron <ian.baron@manchester.ac.uk>
2025-06-07 12:41:29 +0100

    update theme colours, replace shadows with hover effects, flex-wrap header

--- frontend/src/components/Checkbox.vue
+++ frontend/src/components/Checkbox.vue
@@ -26,7 +26,7 @@ const model = computed({
       :id="label"
       v-model="model"
       type="checkbox"
-      class="accent-slate-600 dark:accent-slate-400"
+      class="accent-zinc-600 dark:accent-zinc-400"
     >
   </div>
 </template>
--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   shortcuts: {
-    background: 'bg-slate-100 dark:bg-slate-900',
-    recipeCardBackground: 'bg-white dark:bg-slate-800',
-    border: 'border-1 border-solid border-slate-300 dark:border-slate-600',
-    text: 'text-slate-700 dark:text-slate-200',
-    recipeCardText: 'text-slate-600 dark:text-slate-300',
-    headerButton: 'size-12 bg-white dark:bg-slate-700 overflow-hidden border border-slate-300 dark:border-slate-600 rounded hover:cursor-pointer text-slate-700 dark:text-slate-200 transition-colors transition-transform duration-150 ease-in-out hover:bg-slate-200 dark:hover:bg-slate-600 active:scale-95 active:bg-slate-300 dark:active:bg-slate-500',
-    textButton: 'size-12 bg-white dark:bg-slate-700 overflow-hidden border border-slate-300 dark:border-slate-600 rounded text-slate-700 dark:text-slate-200 transition-colors transition-transform duration-150 ease-in-out hover:bg-slate-200 dark:hover:bg-slate-600 active:scale-95 active:bg-slate-300 dark:active:bg-slate-500',
-    headerSearch: 'h-11 text-xl focus:placeholder-op-0 focus:bg-slate-300 dark:focus:bg-slate-600 bg-white dark:bg-slate-700 text-slate-700 dark:text-slate-200 text-center md:text-2xl px-2 outline-none w-1/2 md:w-1/4 placeholder-slate-400 dark:placeholder-slate-400',
-    headerButtonIcon: 'text-2xl align-middle text-center text-slate-700 dark:text-slate-200',
-    titleText: 'text-slate-800 dark:text-slate-100',
-    filterHr: 'w-80% my-4 border-slate-300 dark:border-slate-600 border-1 border-solid',
+    background: 'bg-zinc-100 dark:bg-zinc-800',
+    recipeCardBackground: 'bg-white dark:bg-zinc-700',
+    border: 'border-1 border-solid border-zinc-300 dark:border-zinc-600',
+    text: 'text-zinc-700 dark:text-zinc-200',
+    recipeCardText: 'text-zinc-600 dark:text-zinc-300',
+    headerButton: 'size-12 bg-white dark:bg-zinc-700 overflow-hidden border border-zinc-300 dark:border-zinc-600 rounded hover:cursor-pointer text-zinc-700 dark:text-zinc-200 transition-colors transition-transform duration-150 ease-in-out hover:bg-zinc-200 dark:hover:bg-zinc-600 active:scale-95 active:bg-zinc-300 dark:active:bg-zinc-500',
+    textButton: 'size-12 bg-white dark:bg-zinc-700 overflow-hidden border border-zinc-300 dark:border-zinc-600 rounded text-zinc-700 dark:text-zinc-200 transition-colors transition-transform duration-150 ease-in-out hover:bg-zinc-200 dark:hover:bg-zinc-600 active:scale-95 active:bg-zinc-300 dark:active:bg-zinc-500',
+    headerSearch: 'h-11 text-xl focus:placeholder-op-0 focus:bg-zinc-300 dark:focus:bg-zinc-600 bg-white dark:bg-zinc-700 text-zinc-700 dark:text-zinc-200 text-center md:text-2xl px-2 outline-none w-1/2 md:w-1/4 placeholder-zinc-400 dark:placeholder-zinc-400',
+    headerButtonIcon: 'text-2xl align-middle text-center text-zinc-700 dark:text-zinc-200',
+    titleText: 'text-zinc-800 dark:text-zinc-100',
+    filterHr: 'w-80% my-4 border-zinc-300 dark:border-zinc-600 border-1 border-solid',
   },
   theme: {
     colors: {},
